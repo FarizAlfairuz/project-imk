641bfbb107a9e61964fe0d600ac03336b25638ae
FarizAlfairuz <farizalfairuz27@gmail.com>
2021-05-04 06:59:00 +0700

    baru

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        Vector3 move = transform.right * x;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
